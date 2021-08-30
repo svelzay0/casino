6f491b22273dd0cc9961815420ce212d864d65d6
svelzay0 <pavelkoshevoi@yandex.ru>
2021-08-31 00:17:21 +0300

    V-07

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/styles.scss'
+import "./styles/theme/index.css";
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/ru-RU";
+
+Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false
 
---
+++ src/views/HomePage.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="homepage__container">
+    <div class="form__container">
+      <header class="homepage__header">
+        <img 
+          class="header__img" 
+          :src="require('@/assets/logo.svg')" 
+          alt="" 
+        />
+        <h1 class="header_title">Need for drive</h1>
+      </header>
+      <el-card class="homepage__card">
+        <el-form label-position="top">
+          <p class="form__title">Вход</p>
+          <el-form-item label="Почта">
+            <el-input 
+              v-model="form.email" 
+              size="medium" 
+            />
+          </el-form-item>
+          <el-form-item label="Пароль">
+            <el-input 
+              v-model="form.password"
+              size="medium"
+            />
+          </el-form-item>
+          <div class="form__footer">
+            <el-link 
+              class="form__footer_anchor" 
+              type="primary"
+            >
+              Запросить доступ
+            </el-link>
+            <el-button 
+              size="small"
+              @click="validation"
+            >
+              Войти
+            </el-button>
+          </div>
+        </el-form>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      wrongEmail: false,
+      wrongPass: false,
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    validation () {
+      if (this.form.email != 'admin@m.ru') {
+        this.wrongEmail = true
+      }
+      if (this.form.password != 'password') {
+        this.wrongPass = true
+      }
+    }
+  }
+}
+</script>
