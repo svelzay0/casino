e5ddeda45db50d8860a696352b853fdffb34f0dc
svelzay0 <pavelkoshevoi@yandex.ru>
2021-09-25 00:40:03 +0300

    fix

--- src/pages/admin/Orders.vue
+++ src/pages/admin/Orders.vue
@@ -4,6 +4,8 @@
       <v-data-table
         :headers="orderHeaders"
         :items="orderItems"
+        :loading="loadBool"
+        loading-text="Загрузка заказов, пожалуйста подождите..."
         item-key="id"
         class="elevation-2"
       >
@@ -14,20 +16,46 @@
         </template> -->
         <template #item.info="{ item }">
           <v-row>
-            <v-col>
-              {{ item.cityId ? item.cityId.name : '-' }}
+            <v-col cols="12" class="pb-0 mb-0">
+              г. {{ item.cityId ? item.cityId.name : '-' }}
             </v-col>
-            <v-col>
-              {{ item.dateFrom ? item.dateFrom : '-' }} - {{ item.dateTo ? item.dateTo : '-' }}
+            <v-col cols="12"  class="ma-0 pa-0 pl-3">
+              {{ item.dateFrom ? formatDate(item.dateFrom) : '-' }} - {{ item.dateTo ? formatDate(item.dateTo) : '-' }}
+            </v-col>
+            <v-col cols="12" class="pt-0 mt-0">
+              Цвет <b>{{ item.color ? item.color : '-' }}</b>
             </v-col>
           </v-row>
         </template>
-        <!-- <template #item.car="{ item }">
-          <div>
-            {{ item.carId.name }}
-          </div>
+        <template #item.car="{ item }">
+          <v-row>
+            <v-col cols="12" class="pb-0 mb-0">
+              Модель {{ item.carId ? item.carId.name : '-' }}
+            </v-col>
+            <v-col cols="12"  class="ma-0 pa-0 pl-3">
+              Категория {{ item.carId ? (item.carId.categoryId ? item.carId.categoryId.description : '-') : '-' }}
+            </v-col>
+            <v-col cols="12" class="pt-0 mt-0">
+              Номер <b class="primary--text">{{ item.carId ? item.carId.number : '-' }}</b>
+            </v-col>
+          </v-row>
         </template>
-        <template #item.category="{ item }">
+        <template #item.additionals="{ item }">
+          <v-row>
+            <v-col cols="12" class="pa-0 ma-0">
+              <v-checkbox
+              v-model="item.isFullTank" 
+              :label="'Полный бак'"></v-checkbox>
+            </v-col>
+            <v-col cols="12" class="pa-0 ma-0">
+              <v-checkbox v-model="item.isNeedChildChair" :label="'Детское кресло'"></v-checkbox>
+            </v-col>
+            <v-col cols="12" class="pa-0 ma-0">
+              <v-checkbox v-model="item.isRightWheel" :label="'Правый руль'"></v-checkbox>
+            </v-col>
+          </v-row>
+        </template>
+        <!-- <template #item.category="{ item }">
           <div>
             {{ item.carId.categoryId.description + ' - ' +  item.carId.categoryId.name }}
           </div>
@@ -57,6 +85,7 @@
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import moment from "moment";
 
 export default {
   name: 'Orders',
@@ -64,18 +93,13 @@ export default {
   data() {
     return {
       orderItems: [],
-      imgDefPath: require('@/assets/default_car.jpg')
+      imgDefPath: require('@/assets/default_car.jpg'),
+      loadBool: true
     }
   },
   computed: {
     orderHeaders () {
       return [
-        // {
-        //   value: 'id',
-        //   text: 'ID',
-        //   class: 'text-no-wrap',
-        //   width: '1%'
-        // },
         {
           value: 'photo',
           text: 'Фото',
@@ -85,7 +109,9 @@ export default {
         {
           value: 'info',
           text: 'Информация',
-          searchable: false
+          searchable: false,
+          sortable: false,
+          width: '20%'
         },
         {
           value: 'car',
@@ -94,40 +120,8 @@ export default {
           sortable: false
         },
         {
-          value: 'category',
-          text: 'Категория'
-        },
-        {
-          value: 'number',
-          text: 'Номер'
-        },
-        {
-          value: 'color',
-          text: 'Цвет авто',
-          searchable: false,
-          sortable: false
-        },
-        {
-          value: 'scan_received',
-          text: 'Скан получен',
-          searchable: false,
-          sortable: false
-        },
-        {
-          value: 'scan_url',
-          text: '',
-          searchable: false,
-          sortable: false
-        },
-        {
-          value: 'original_sent',
-          text: 'Оригал отправлен',
-          searchable: false,
-          sortable: false
-        },
-        {
-          value: 'original_received',
-          text: 'Оригал получен',
+          value: 'additionals',
+          text: 'Особенности',
           searchable: false,
           sortable: false
         },
@@ -144,11 +138,18 @@ export default {
   watch: {
     'getOrders': function (newVal) {
       this.orderItems = newVal
-      console.log(6, this.orderItems)
-    },
+    }
   },
   mounted() {
-    this.fetchOrders();
+    if (this.getOrders.length === 0) {
+      this.fetchOrders().then(() => {
+        this.loadBool = false;
+        this.orderItems = this.getOrders;
+      });
+    } else {
+      this.orderItems = this.getOrders;
+      this.loadBool = false;
+    }
   },
   methods: {
     ...mapActions("orders",
@@ -156,13 +157,13 @@ export default {
         "fetchOrders"
       ]),
     getImgPath(car) {
-      if (typeof(car) != "undefined" && car !== null) {
+      if (typeof(car) != "undefined" && car !== null && car.thumbnail.path.length < 1000) {
         return `${process.env.VUE_APP_API_IMG}${car.thumbnail.path}`;
-      }
-      else return this.imgDefPath
-    }
-  }
-  
-  
+      } else return this.imgDefPath
+    },
+    formatDate (date) {
+      return date ? moment(date).format('DD.MM.YYYY h:mm') : ''
+    },
+  } 
 };
 </script>
